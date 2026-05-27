ec8083175063c9c59084519ef5797437a624bf6b
EugenBonta <e.bonta03@gmail.com>
2026-05-27 20:13:55 +0300

    test ci/cd

--- src/main/java/com/example/requesttracker/controller/RequestLogController.java
+++ src/main/java/com/example/requesttracker/controller/RequestLogController.java
@@ -20,7 +20,7 @@ public class RequestLogController {
 
     @GetMapping("/hi")
     public String hello() {
-        return "Hello World";
+        return "Hello Cruel World";
     }
 
     @GetMapping("/all")
